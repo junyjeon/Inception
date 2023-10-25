b6073eacea96af2c31fc9761f94489d8cf167ffe
junyjeon <jjy8479@gmail.com>
2023-10-26 06:15:31 +0900

    1

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 환경 변수를 사용하여 사용자 비밀번호 설정
-USER1_PASSWORD=${USER1_PASSWORD:-password1}
-USER2_PASSWORD=${USER2_PASSWORD:-password2}
+USER1_PASSWORD=${MYSQL_PASSWORD:-password1}
+USER2_PASSWORD=${MYSQL_PASSWORD:-password2}
 
 # MariaDB 서버 시작
 mysqld_safe &
@@ -13,15 +13,15 @@ while ! mysqladmin ping --silent; do
 done
 
 # 사용자 생성
-mysql -e "CREATE USER 'user1'@'%' IDENTIFIED BY '${USER1_PASSWORD}';"
-mysql -e "CREATE USER 'user2'@'%' IDENTIFIED BY '${USER2_PASSWORD}';"
+mysql -e "CREATE USER '${MYSQL_USER1}'@'%' IDENTIFIED BY '${USER1_PASSWORD}';"
+mysql -e "CREATE USER '${MYSQL_USER2}'@'%' IDENTIFIED BY '${USER2_PASSWORD}';"
 
 # 데이터베이스 생성
-mysql -e "CREATE DATABASE wordpress;"
+mysql -e "CREATE DATABASE ${MYSQL_DATABASE};"
 
 # 사용자에게 데이터베이스 권한 부여
-mysql -e "GRANT ALL ON wordpress.* TO 'user1'@'%';"
-mysql -e "GRANT ALL ON wordpress.* TO 'user2'@'%';"
+mysql -e "GRANT ALL ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER1}'@'%';"
+mysql -e "GRANT ALL ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER2}'@'%';"
 
 # 변경 사항 적용
 mysql -e "FLUSH PRIVILEGES;"
