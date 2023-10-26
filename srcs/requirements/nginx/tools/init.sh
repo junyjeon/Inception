cd2d83fc2bc8915892002e3d530e55c66c75a7b7
junyjeon <jjy8479@gmail.com>
2023-10-27 00:53:52 +0900

    7

--- srcs/requirements/nginx/tools/init.sh
+++ srcs/requirements/nginx/tools/init.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
+echo "Generating certificate for ${INTRA_ID}.42.fr"
+
 openssl req -newkey rsa:2048 -days 365 -nodes -x509 \
-    -subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/CN=${DOMAIN_NAME}" \
-    -keyout "/etc/ssl/${DOMAIN_NAME}.key" \
-    -out "/etc/ssl/${DOMAIN_NAME}.crt" 2>/dev/null
+-subj "/C=KR/ST=Seoul/L=Gangnam-gu/O=42Seoul/CN=${INTRA_ID}.42.fr" \
+-keyout "/etc/ssl/${INTRA_ID}.42.fr.key" \
+-out "/etc/ssl/${INTRA_ID}.42.fr.crt"
+
+if [ $? -ne 0 ]; then
+echo "Failed to generate certificate"
+exit 1
+fi
+
+ls -l /etc/ssl
 
 sed -i "s/\${DOMAIN_NAME}/${DOMAIN_NAME}/g" /etc/nginx/nginx.conf
 
