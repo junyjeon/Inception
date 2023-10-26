216f1e90e94408b68a54cb0b5ac7622eef91e6d4
junyojeo <jjy8479@gmail.com>
2023-10-27 07:01:01 +0900

    1

--- Makefile
+++ Makefile
@@ -1,6 +1,13 @@
+INTRA_ID=junyojeo
+DB_VOLUME=/home/${INTRA_ID}/data/mariadb
+WORDPRESS_VOLUME=/home/${INTRA_ID}/data/wordpress
+
 all: build up
 
 build:
+	sudo mkdir -p ${DB_VOLUME}
+	sudo mkdir -p ${WORDPRESS_VOLUME}
+	sudo chown -R junyojeo:junyojeo /home/junyojeo/data
 	sudo docker build -t nginx:1.24.0 srcs/requirements/nginx
 	sudo docker build -t wordpress:6.3.2 srcs/requirements/wordpress
 	sudo docker build -t mariadb:11.1.2 srcs/requirements/mariadb
--- srcs/requirements/nginx/tools/init.sh
+++ srcs/requirements/nginx/tools/init.sh
@@ -2,11 +2,6 @@
 
 echo "Generating certificate for ${INTRA_ID}.42.fr"
 
-openssl req -newkey rsa:2048 -days 365 -nodes -x509 \
--subj "/C=KR/ST=Seoul/L=Gangnam-gu/O=42Seoul/CN=${INTRA_ID}.42.fr" \
--keyout "/etc/ssl/${INTRA_ID}.42.fr.key" \
--out "/etc/ssl/${INTRA_ID}.42.fr.crt"
-
 if [ $? -ne 0 ]; then
 echo "Failed to generate certificate"
 exit 1
@@ -14,7 +9,7 @@ fi
 
 ls -l /etc/ssl
 
-sed -i "s/\${DOMAIN_NAME}/${DOMAIN_NAME}/g" /etc/nginx/nginx.conf
+# sed -i "s/\${DOMAIN_NAME}/${DOMAIN_NAME}/g" /etc/nginx/nginx.conf
 
 echo "nginx ON, port is 443"
 
