79518c50e5c81e6c7b8c315633c3828b02853c92
junyojeo <junyojeo@junyojeo.myguest.virtualbox.org>
2023-11-14 20:04:29 +0900

    1

--- srcs/requirements/nginx/tools/nginx.sh
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -1,11 +1,11 @@
 openssl req -x509 -nodes \
         -days 365 \
         -newkey rsa:2048 \
-        -keyout /etc/ssl/nginx-selfsigned.key \
-        -out /etc/ssl/nginx-selfsigned.crt \
+        -keyout /etc/ssl/private/nginx-selfsigned.key \
+        -out /etc/ssl/certs/nginx-selfsigned.crt \
         -subj "/C=KR/ST=Seoul/O=42Seoul/OU=Cadet/CN=junyojeo.42.fr/"
-    
-chown nginx /etc/ssl/nginx-selfsigned.crt    
-chown nginx /etc/ssl/nginx-selfsigned.key    
+
+chown nginx /etc/ssl/private/nginx-selfsigned.key
+chown nginx /etc/ssl/certs/nginx-selfsigned.crt
 
 exec nginx -g 'daemon off;';
